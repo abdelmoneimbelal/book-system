ff6f15c8d8d4f7f6bf4aabc6b96bfaeeb65cb8c5
Abdelmoneim Belal <38110521+abdelmoneimbelal@users.noreply.github.com>
2025-12-29 14:56:48 +0200

    Create Category slug

--- app/Filament/Admin/Resources/Categories/Pages/CreateCategory.php
+++ app/Filament/Admin/Resources/Categories/Pages/CreateCategory.php
@@ -2,10 +2,27 @@
 
 namespace App\Filament\Admin\Resources\Categories\Pages;
 
-use App\Filament\Admin\Resources\Categories\CategoryResource;
+use App\Models\Category;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
 use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Admin\Resources\Categories\CategoryResource;
 
 class CreateCategory extends CreateRecord
 {
     protected static string $resource = CategoryResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['slug'] = Str::slug($data['name'], '-') . '-' . Str::random(5);
+
+        return $data;
+    }
+
+    // protected function handleRecordCreation(array $data): Model
+    // {
+    //     $category = Category::create($data);
+
+    //     return $category;
+    // }
 }

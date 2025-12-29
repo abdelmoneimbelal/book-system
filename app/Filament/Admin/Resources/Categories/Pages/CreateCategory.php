ba9faa0b3ab85d49f945942d30eca9b5b87e9486
Abdelmoneim Belal <38110521+abdelmoneimbelal@users.noreply.github.com>
2025-12-29 16:00:27 +0200

    preserveFormDataWhenCreatingAnother

--- app/Filament/Admin/Resources/Categories/Pages/CreateCategory.php
+++ app/Filament/Admin/Resources/Categories/Pages/CreateCategory.php
@@ -2,13 +2,13 @@
 
 namespace App\Filament\Admin\Resources\Categories\Pages;
 
-use App\Models\Category;
-use Illuminate\Support\Str;
-use Illuminate\Database\Eloquent\Model;
-use Filament\Resources\Pages\CreateRecord;
 use App\Filament\Admin\Resources\Categories\CategoryResource;
+use App\Models\Category;
 use Filament\Notifications\Notification;
+use Filament\Resources\Pages\CreateRecord;
 use Filament\Support\Icons\Heroicon;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class CreateCategory extends CreateRecord
 {
@@ -16,7 +16,7 @@ class CreateCategory extends CreateRecord
 
     protected function mutateFormDataBeforeCreate(array $data): array
     {
-        $data['slug'] = Str::slug($data['name'], '-') . '-' . Str::random(5);
+        $data['slug'] = Str::slug($data['name'], '-').'-'.Str::random(5);
 
         return $data;
     }
@@ -31,10 +31,17 @@ class CreateCategory extends CreateRecord
         return Notification::make()
             ->success()
             ->title('Category created')
-            ->body('The category <strong>' . $this->getRecord()->name . '</strong> has been created successfully.')
+            ->body('The category <strong>'.$this->getRecord()->name.'</strong> has been created successfully.')
             ->icon(Heroicon::ListBullet);
     }
 
+    protected function preserveFormDataWhenCreatingAnother(array $data): array
+    {
+        return [
+            'name' => $data['name'],
+            'slug' => '',
+        ];
+    }
 
     // protected function getRedirectUrl(): string
     // {
@@ -42,7 +49,6 @@ class CreateCategory extends CreateRecord
     //     // return $this->getResource()::getUrl('index');
     //     // return $this->previousUrl ?? $this->getResource()::getUrl('index');
     // }
-
 
     // protected function handleRecordCreation(array $data): Model
     // {

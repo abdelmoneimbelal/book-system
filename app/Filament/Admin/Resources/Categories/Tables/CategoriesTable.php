91c0cad941f3bce980504d57afa10319ea3ce8f0
Abdelmoneim Belal <38110521+abdelmoneimbelal@users.noreply.github.com>
2026-01-04 12:05:44 +0200

    SoftDeletes

--- app/Filament/Admin/Resources/Categories/Tables/CategoriesTable.php
+++ app/Filament/Admin/Resources/Categories/Tables/CategoriesTable.php
@@ -3,14 +3,17 @@
 namespace App\Filament\Admin\Resources\Categories\Tables;
 
 use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
-use Filament\Actions\DeleteAction;
+use Filament\Actions\ForceDeleteAction;
+use Filament\Actions\ForceDeleteBulkAction;
+use Filament\Actions\RestoreAction;
+use Filament\Actions\RestoreBulkAction;
 use Filament\Actions\ViewAction;
-use Filament\Tables\Table;
 use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Columns\IconColumn;
-use Filament\Tables\Columns\ToggleColumn;
+use Filament\Tables\Filters\TrashedFilter;
+use Filament\Tables\Table;
 
 class CategoriesTable
 {
@@ -19,17 +22,17 @@ class CategoriesTable
         return $table
             ->columns([
                 TextColumn::make('name')
-                ->searchable()
-                ->sortable(),
+                    ->searchable()
+                    ->sortable(),
                 TextColumn::make('status')
-                ->badge()
-                ->color(function ($record) {
-                    return $record->status === 'active' ? 'success' : 'danger';
-                })
-                ->sortable(),
+                    ->badge()
+                    ->color(function ($record) {
+                        return $record->status === 'active' ? 'success' : 'danger';
+                    })
+                    ->sortable(),
                 TextColumn::make('created_at')
-                ->dateTime()
-                ->sortable(),
+                    ->dateTime()
+                    ->sortable(),
                 // ->toggleable(isToggledHiddenByDefault: true),
                 // TextColumn::make('updated_at')
                 // ->dateTime()
@@ -37,16 +40,20 @@ class CategoriesTable
                 // ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
-                //
+                TrashedFilter::make(),
             ])
             ->recordActions([
                 ViewAction::make(),
                 EditAction::make(),
                 DeleteAction::make(),
+                ForceDeleteAction::make(),
+                RestoreAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
                     DeleteBulkAction::make(),
+                    ForceDeleteBulkAction::make(),
+                    RestoreBulkAction::make(),
                 ]),
             ]);
     }

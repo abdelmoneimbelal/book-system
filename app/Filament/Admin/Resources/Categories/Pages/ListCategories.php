4665ca30d7e6fbea1f2655a0677b7ce10877d9c1
Abdelmoneim Belal <38110521+abdelmoneimbelal@users.noreply.github.com>
2026-01-14 11:49:10 +0200

    Update ListCategories.php

--- app/Filament/Admin/Resources/Categories/Pages/ListCategories.php
+++ app/Filament/Admin/Resources/Categories/Pages/ListCategories.php
@@ -3,11 +3,11 @@
 namespace App\Filament\Admin\Resources\Categories\Pages;
 
 use App\Filament\Admin\Resources\Categories\CategoryResource;
+use App\Models\Category;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Schemas\Components\Tabs\Tab;
 use Illuminate\Database\Eloquent\Builder;
-use App\Models\Category;
 
 class ListCategories extends ListRecords
 {
@@ -21,21 +21,21 @@ class ListCategories extends ListRecords
     }
 
     public function getTabs(): array
-{
-    return [
-        'all' => Tab::make()
-        ->icon('heroicon-o-list-bullet')
-        ->badge(Category::count())
-        ->badgeTooltip('Total Categories')
-        ->badgeColor('success'),
-        'active' => Tab::make()
-            ->icon('heroicon-o-check-circle')
-            ->badge(Category::where('status', 'active')->count()) 
-            ->modifyQueryUsing(fn (Builder $query) => $query->where('status', 'active')),
-        'inactive' => Tab::make()
-            ->icon('heroicon-o-x-circle')
-            ->badge(Category::where('status', 'inactive')->count()) 
-            ->modifyQueryUsing(fn (Builder $query) => $query->where('status', 'inactive')),
-    ];
-}
+    {
+        return [
+            'all' => Tab::make()
+                ->icon('heroicon-o-list-bullet')
+                ->badge(Category::count())
+                ->badgeTooltip('Total Categories')
+                ->badgeColor('success'),
+            'active' => Tab::make()
+                ->icon('heroicon-o-check-circle')
+                ->badge(Category::where('status', 'active')->count())
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', 'active')),
+            'inactive' => Tab::make()
+                ->icon('heroicon-o-x-circle')
+                ->badge(Category::where('status', 'inactive')->count())
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', 'inactive')),
+        ];
+    }
 }

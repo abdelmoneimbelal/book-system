9ad199663e7beb3711f4fef93abf464537bb5479
Abdelmoneim Belal <38110521+abdelmoneimbelal@users.noreply.github.com>
2026-01-18 13:33:00 +0200

    Update UserForm.php

--- app/Filament/Admin/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Admin/Resources/Users/Schemas/UserForm.php
@@ -13,14 +13,18 @@ class UserForm
         return $schema
             ->components([
                 TextInput::make('name')
+                    ->placeholder('Enter name')
                     ->required(),
                 TextInput::make('email')
                     ->label('Email address')
+                    ->placeholder('Enter email address')
                     ->email()
                     ->required(),
                 DateTimePicker::make('email_verified_at'),
                 TextInput::make('password')
                     ->password()
+                    ->revealable()
+                    ->placeholder('Enter password')
                     ->required(),
             ]);
     }

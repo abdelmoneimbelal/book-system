9ca751c64430f441e28477d5778128c0b1fa0073
Abdelmoneim Belal <38110521+abdelmoneimbelal@users.noreply.github.com>
2026-04-01 10:33:41 +0200

    Update CreateBorrowing.php

--- app/Filament/Admin/Resources/Borrowings/Pages/CreateBorrowing.php
+++ app/Filament/Admin/Resources/Borrowings/Pages/CreateBorrowing.php
@@ -4,8 +4,19 @@ namespace App\Filament\Admin\Resources\Borrowings\Pages;
 
 use App\Filament\Admin\Resources\Borrowings\BorrowingResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreateBorrowing extends CreateRecord
 {
     protected static string $resource = BorrowingResource::class;
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $borrowing = static::getModel()::create($data);
+        $book = $borrowing->book;
+        $book->decrement('available_copies');
+        $book->status = $book->available_copies > 0 ? 'available' : 'unavailable';
+        $book->save();
+        return $borrowing;
+    } 
 }

a483bc61dcc0f403cc76a3faa40e01d716ad8ca1
Abdelmoneim Belal <38110521+abdelmoneimbelal@users.noreply.github.com>
2026-04-01 12:23:45 +0200

    Custom Widget

---
+++ resources/views/filament/widgets/my-custom-widget.blade.php
@@ -0,0 +1,10 @@
+<x-filament-widgets::widget>
+    <x-filament::section>
+        {{-- <h1>My Custom Widget</h1> --}}
+        {{-- Widget content --}}
+        <form wire:submit.prevent="submit">
+            <x-filament::input wire:model.defer='email' label="Email" style="border: 1px solid red;" placeholder="Email" />
+            <x-filament::button type="submit" style="margin-top: 10px;">Submit</x-filament::button>
+        </form>
+    </x-filament::section>
+</x-filament-widgets::widget>

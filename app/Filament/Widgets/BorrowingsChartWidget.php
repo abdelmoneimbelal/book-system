2430b67cb8fafcc76ae4c971595700540a2ba808
Abdelmoneim Belal <38110521+abdelmoneimbelal@users.noreply.github.com>
2026-04-06 12:36:52 +0200

    Update BorrowingsChartWidget.php

--- app/Filament/Widgets/BorrowingsChartWidget.php
+++ app/Filament/Widgets/BorrowingsChartWidget.php
@@ -2,16 +2,33 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Borrowing;
 use Filament\Widgets\ChartWidget;
 
 class BorrowingsChartWidget extends ChartWidget
 {
     protected ?string $heading = 'Borrowings Chart Widget';
 
+    protected ?string $maxHeight = '200px';
+
+    protected int | string | array $columnSpan = 'full';
+
     protected function getData(): array
     {
+        // $borrowings = Borrowing::query()
+        //         ->selectRaw('DATE(borrowed_at) as borrowed_date, COUNT(*) as total')
+        //         ->groupBy('borrowed_date')
+        //         ->get();
+
         return [
-            //
+            'datasets' => [
+                [
+                    'label' => 'Borrowings',
+                    'data' => [0, 10, 5, 2, 21, 32, 45, 74, 65, 45, 77, 89],
+                    // 'data' => $borrowings->pluck('total'),
+                ],
+            ],
+            'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
         ];
     }
 
@@ -19,4 +36,8 @@ class BorrowingsChartWidget extends ChartWidget
     {
         return 'line';
     }
+
+    protected string $color = 'info';
+
+    protected bool $isCollapsible = true;   
 }
